a01ce1119fba68d0b955ef7dc535a21a0fa28c23
Ömer Taha Öztop <omertahaoztop@gmail.com>
2026-09-17 01:55:14 +0300

    Fix uptime justification to column 64

--- today.py
+++ today.py
@@ -327,7 +327,7 @@ def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib
     """
     tree = etree.parse(filename)
     root = tree.getroot()
-    justify_format(root, 'age_data', age_data, 54)
+    justify_format(root, 'age_data', age_data, 55)
     justify_format(root, 'commit_data', commit_data, 23)
     justify_format(root, 'star_data', star_data, 18)
     justify_format(root, 'repo_data', repo_data, 6)
